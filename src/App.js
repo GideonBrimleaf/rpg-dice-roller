a3fc10a1720b65de545427d5031bb63fe05d8aaa
GideonBrimleaf <chrisdryden1@gmail.com>
2020-07-01 15:34:59 +0100

    fixed bug removing ids on dice roll

--- src/App.js
+++ src/App.js
@@ -16,22 +16,22 @@ function App() {
 
   const rollAllDice = () => {
     const newRoll = dice.map( 
-      () => ({"currentValue" : diceValues[Math.floor(Math.random() * diceValues.length)]})
+      (dice) => ({"id": dice.id, "currentValue" : diceValues[Math.floor(Math.random() * diceValues.length)]})
     )
     setDice(newRoll)
   }
 
   const diceResults = dice.map( dice => {
-    return (<Dice key={dice.id} currentValue = {dice.currentValue} />)
+    return (<Dice key={dice.id} currentValue={dice.currentValue} />)
   }
   )
 
   return (
-    <div className="App">
-      <main className="App-main">
-        <p>
-          I'm Mary Poppins Y'All!
-        </p>
+    <div className="App-main">
+      <header>
+        Welcome to the Fate Dice Roller!
+      </header>
+      <main>
         <button className="Dice-Roller" onClick={rollAllDice}>Roll all Dice</button>
         <ul>
           {diceResults}
